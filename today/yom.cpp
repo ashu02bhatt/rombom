016abde56fb0fa493514d2ef4594ee73bed7caf4
sam <sdjfb@sdf>
2019-12-04 12:17:02 -0800

    new comittttt

---
+++ today/yom.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a,b,c;
+	cout<<"enter two  numbers ";
+	cin>>a>>b;
+	c=a+b;
+	cout<<"addition of two numbers is c:"<<c;
+
+	return 0;
+}
